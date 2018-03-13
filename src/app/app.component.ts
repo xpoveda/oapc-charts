01353a11ea3df4b235581eccbdf83a4e7af62af4
xpoveda <xpoveda@gmail.com>
2018-03-13 18:43:24 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component }    from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+
+import { LineChartDemoComponent} from './line-chart-demo/line-chart-demo.component'
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent }           from './app.component';
+import { ChartsModule }           from 'ng2-charts';
+import { LineChartDemoComponent } from './line-chart-demo/line-chart-demo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
